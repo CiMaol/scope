001a65f214f7d7014ffcf9799ce3135bac7d59d8
cimaol <cimaolul@gmail.com>
2020-03-09 13:55:00 +0000

    WIP: moving kubelet client to TLS secure port for pod listing when node name is not known

--- probe/kubernetes/kubelet.go
+++ probe/kubernetes/kubelet.go
@@ -1,7 +1,10 @@
 package kubernetes
 
 import (
+	"crypto/tls"
 	"fmt"
+	"github.com/weaveworks/scope/vendor/k8s.io/client-go/transport"
+	restclient "k8s.io/client-go/rest"
 	"net/http"
 
 	"github.com/ugorji/go/codec"
@@ -17,10 +20,17 @@ type podList struct {
 	} `json:"items"`
 }
 
+// kubeletClientConfig contains the TLS configuration to get access to the kubelet endpoint with TLS enabled
+type kubeletClientConfig struct {
+	restclient.TLSClientConfig
+}
+
 // GetLocalPodUIDs obtains the UID of the pods run locally (it's just exported for testing)
 var GetLocalPodUIDs = func(kubeletHost string) (map[string]struct{}, error) {
-	url := fmt.Sprintf("http://%s/pods/", kubeletHost)
-	resp, err := http.Get(url)
+	url := fmt.Sprintf("https://%s/pods/", kubeletHost)
+	config := kubeletClientConfig{}
+	client := kubeletClient(config)
+	resp, err := client.Get(url)
 	if err != nil {
 		return nil, err
 	}
@@ -34,4 +44,19 @@ var GetLocalPodUIDs = func(kubeletHost string) (map[string]struct{}, error) {
 		result[pod.Metadata.UID] = struct{}{}
 	}
 	return result, nil
+}
+
+func kubeletClient(config kubeletClientConfig) *http.Client {
+	cfg := &transport.Config{
+		TLS: transport.TLSConfig{
+			CAFile:   config.CAFile,
+			CAData:   config.CAData,
+			CertFile: config.CertFile,
+			CertData: config.CertData,
+			KeyFile:  config.KeyFile,
+			KeyData:  config.KeyData,
+		},
+	}
+	transport := http.Transport{TLSClientConfig: cfg}
+	return &http.Client{}
 }
